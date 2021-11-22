e984ff24f057ad779f2302b596287653fd51ed10
Tom Wiesing <tkw01536@gmail.com>
2021-11-22 14:08:30 +0100

    kgp: Use reflect.DeepEqual in tests

--- server/go-kgp/board_test.go
+++ server/go-kgp/board_test.go
@@ -1,33 +1,9 @@
 package main
 
-import "testing"
-
-func boardEq(b1 *Board, b2 *Board) bool {
-	if b1 == nil || b2 == nil {
-		return false
-	}
-
-	if b1.north != b2.north {
-		return false
-	}
-	if b1.south != b2.south {
-		return false
-	}
-
-	for i := range b1.northPits {
-		if b1.northPits[i] != b1.northPits[i] {
-			return false
-		}
-	}
-
-	for i := range b1.southPits {
-		if b1.southPits[i] != b1.southPits[i] {
-			return false
-		}
-	}
-
-	return true
-}
+import (
+	"reflect"
+	"testing"
+)
 
 func TestLegal(t *testing.T) {
 	for i, test := range []struct {
@@ -254,7 +230,7 @@ func TestSow(t *testing.T) {
 		again := test.start.Sow(test.side, test.move)
 		if test.again != again {
 			t.Errorf("(%d) Didn't recognize repeat move", i)
-		} else if !boardEq(test.start, test.end) {
+		} else if !reflect.DeepEqual(test.start, test.end) {
 			t.Errorf("(%d) Expected %s, got %s", i, test.end, test.start)
 		}
 	}
