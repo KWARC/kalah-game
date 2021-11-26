e9d1b75986efbd986a2f9444761c3098e56c3955
Philip Kaludercic <philip.kaludercic@fau.de>
2021-11-26 16:56:00 +0100

    go-kgp: Don't attempt to resend messages over websocket

--- server/go-kgp/client.go
+++ server/go-kgp/client.go
@@ -95,6 +95,11 @@ func (cli *Client) Respond(to uint64, command string, args ...interface{}) uint6
 retry:
 	n, err := io.Copy(cli.rwc, buf)
 	if err != nil {
+		_, isWS := cli.rwc.(*wsrwc)
+		if isWS {
+			return id
+		}
+
 		nerr, ok := err.(net.Error)
 		if i > 0 && (!ok || (ok && nerr.Temporary())) {
 			time.Sleep(10 * time.Millisecond)
