eda912005b530457d4bec2e14f299df3324b3ba2
Philip Kaludercic <philip.kaludercic@fau.de>
2021-12-04 13:40:36 +0100

    go-kgp: Ignore games that aren't over in game listings

--- server/go-kgp/sql/select-games-by.sql
+++ server/go-kgp/sql/select-games-by.sql
@@ -2,7 +2,7 @@
 
 SELECT id, size, init, north, south, outcome, start, end
 FROM game
-WHERE north == ?1 OR south == ?1
+WHERE (north == ?1 OR south == ?1) AND end IS NOT NULL
 ORDER BY start
 LIMIT 100
 OFFSET ? * 100;
--- server/go-kgp/sql/select-games.sql
+++ server/go-kgp/sql/select-games.sql
@@ -2,6 +2,7 @@
 
 SELECT id, size, init, north, south, outcome, start, end
 FROM game
+WHERE end IS NOT NULL
 ORDER BY start DESC
 LIMIT ?2
 OFFSET ?1 * ?2;
