1a2ecfd5a07449aaf7fdb0c4c6ee4aff47ee9f12
Philip Kaludercic <philipk@posteo.net>
2021-12-12 11:46:57 +0100

    go-kgp: Add license headers to every file

--- server/go-kgp/queue.go
+++ server/go-kgp/queue.go
@@ -1,3 +1,22 @@
+// Client Waiting Queue 
+//
+// Copyright (c) 2021  Philip Kaludercic
+//
+// This file is part of go-kgp.
+//
+// go-kgp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License,
+// version 3, as published by the Free Software Foundation.
+//
+// go-kgp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License, version 3, along with go-kgp. If not, see
+// <http://www.gnu.org/licenses/>
+
 package main
 
 import (
