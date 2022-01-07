c1f47f8834e3e80cd922ef4f8fc38d1697889d20
Philip Kaludercic <philip.kaludercic@fau.de>
2022-01-07 18:19:54 +0100

    go-kgp: Simplify queue assumptions
    By ensuring that there are no duplicates in the queue (as might be
    necessary for a bot playing multiple games in freeplay mode), the more
    complicated remove function can be replaced by a simple iteration.
    
    This corresponds to the current assumptions around Game.Start, that
    require the client to play only one game at a time.

--- server/go-kgp/queue.go
+++ server/go-kgp/queue.go
@@ -94,66 +94,6 @@ func match(queue []*Client) []*Client {
 	return queue
 }
 
-func remove(cli *Client, queue []*Client) []*Client {
-	// Traverse the queue and replace any reference to CLI with a nil
-	// pointer.
-	found := false
-	for i := range queue {
-		if queue[i] == nil {
-			panic(fmt.Sprintf("Nil reference found in queue (%d): %v", i, queue))
-		}
-		if queue[i] == cli {
-			queue[i] = nil
-			found = true
-		}
-	}
-
-	// In case CLI was not in the queue, we return it unmodified
-	if !found {
-		return queue
-	}
-
-	// To avoid copying the contents of the entire queue, we will just
-	// pull clients from the back of the queue to the beginning.  The
-	// queue therefore is does not preserve a real "real" FIFO
-	// ordering, but this is not necessary and might be argued to
-	// introduce some dynamism into the system.
-
-	// We traverse the queue from the beginning to the end, pulling
-	// clients from the end of the queue to current position (i) to
-	// fill empty spots.
-	for i := 0; i < len(queue); i++ {
-
-		// Before checking if the entry at the current position is
-		// nil, we want to ensure that the last element in the queue
-		// is not nil.
-		for len(queue) > i && queue[len(queue)-1] == nil {
-			queue = queue[:len(queue)-1]
-		}
-
-		// In case we have removed all elements, we can stop immediately
-		if len(queue) == 0 {
-			break
-		}
-
-		// If the current element is empty, we have either reached the
-		// end of the queue when LEN == I, or there is an element at
-		// the end of the queue (QUEUE[LEN-1]) that can be pulled
-		// ahead.
-		if queue[i] == nil {
-			if len(queue) == i {
-				queue = queue[:len(queue)-1]
-				break
-			} else {
-				queue[i] = queue[len(queue)-1]
-				queue = queue[:len(queue)-1]
-			}
-		}
-	}
-
-	return queue
-}
-
 // Try to organise matches
 func queueManager() {
 	var queue []*Client
@@ -161,9 +101,23 @@ func queueManager() {
 	for {
 		select {
 		case cli := <-enqueue:
-			queue = append(queue, cli)
+			vacant := true
+			for _, c := range queue {
+				if cli == c {
+					vacant = false
+					break
+				}
+			}
+			if vacant {
+				queue = append(queue, cli)
+			}
 		case cli := <-forget:
-			queue = remove(cli, queue)
+			for i, c := range queue {
+				if cli == c {
+					queue = append(queue[:i], queue[i+1:]...)
+					break
+				}
+			}
 		}
 
 		if len(queue) >= 2 {
