KWARC/kalah-game

commit 0ee59792bd18adde8be7d802540dc2dfd471a920

go-kgp: Attempt to detect dead or occupied clients in the queue

server/go-kgp/queue.go
@@ -21,7 +21,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	"math/rand"
 )
@@ -35,9 +34,24 @@ var (
 
 // Attempt to match clients for new games
 func match(queue []*Client) []*Client {
+	for len(queue) > 0 {
+		cli := queue[0]
+		if cli.game != nil && cli.rwc != nil {
+			continue
+		}
+		queue = queue[1:]
+	}
+	if len(queue) == 0 {
+		return nil
+	}
+
 	north := queue[0]
 	for i, cli := range queue[1:] {
 		i += 1
+		if cli.game == nil || cli.rwc == nil {
+			queue = append(queue[:i], queue[i+1:]...)
+			continue
+		}
 		if !bytes.Equal(cli.token, north.token) || cli.token == nil {
 			south := cli
 			queue[i] = queue[len(queue)-1]

commit 26527366b28b7500d3250224f968afe544e1dea7

go-kgp: Fix issue introduced in 0ee5979

server/go-kgp/queue.go
@@ -36,8 +36,8 @@ var (
 func match(queue []*Client) []*Client {
 	for len(queue) > 0 {
 		cli := queue[0]
-		if cli.game != nil && cli.rwc != nil {
-			continue
+		if cli.game == nil && cli.rwc != nil {
+			break
 		}
 		queue = queue[1:]
 	}
@@ -48,7 +48,7 @@ func match(queue []*Client) []*Client {
 	north := queue[0]
 	for i, cli := range queue[1:] {
 		i += 1
-		if cli.game == nil || cli.rwc == nil {
+		if cli.game != nil || cli.rwc == nil {
 			queue = append(queue[:i], queue[i+1:]...)
 			continue
 		}