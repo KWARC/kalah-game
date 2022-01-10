808a7751daebe135ea2fed9ad74929309c4a5cec
Philip Kaludercic <philip.kaludercic@fau.de>
2022-01-10 15:28:27 +0100

    go-kgp: Simplify scheduler specification language
    This replaces the stack operations with an implicit sequencing syntax
    that is easier to read and reason about.

--- server/go-kgp/sched.go
+++ server/go-kgp/sched.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -40,16 +41,8 @@ var (
 // A Scheduler updates the waiting queue and manages games
 type Sched func([]*Client) ([]*Client, bool)
 
-// Discard a tournament
-func noop(queue []*Client) ([]*Client, bool) {
-	for _, cli := range queue {
-		cli.kill()
-	}
-	return nil, false
-}
-
 // Compose multiple scheduling systems into one
-func compose(s ...Sched) Sched {
+func compose(s []Sched) Sched {
 	return func(queue []*Client) ([]*Client, bool) {
 		for {
 			if len(s) == 0 {
@@ -62,18 +55,6 @@ func compose(s ...Sched) Sched {
 			}
 			s = s[1:]
 		}
-	}
-}
-
-// Limit the number of times a scheduler can be used
-func limit(s Sched, n uint) Sched {
-	return func(queue []*Client) ([]*Client, bool) {
-		if n == 0 {
-			return nil, true
-		}
-		n--
-
-		return s(queue)
 	}
 }
 
@@ -100,37 +81,42 @@ func reset(score float64) Sched {
 	})
 }
 
-// Split clients into two different schedulers via PRED
-//
-// If PRED returns true for a client, add it to SA, else SB.
-func partition(sa Sched, sb Sched, pred func(*Client) bool) Sched {
-	return func(queue []*Client) ([]*Client, bool) {
-		aqueue := make([]*Client, 0, len(queue))
-		bqueue := make([]*Client, 0, len(queue))
-
+// Filter out all predicates that don't satisfy PRED
+func filter(pred func(*Client) bool) Sched {
+	return func(queue []*Client) (new []*Client, _ bool) {
 		for _, cli := range queue {
 			if pred(cli) {
-				aqueue = append(aqueue, cli)
-			} else {
-				bqueue = append(bqueue, cli)
+				new = append(new, cli)
 			}
 		}
-
-		ra, aover := sa(aqueue)
-		rb, bover := sb(bqueue)
-		return append(ra, rb...), aover && bover
+		return new, true
 	}
 }
 
-func filter(s Sched, pred func(*Client) bool) Sched {
-	return partition(s, noop, pred)
-}
-
 // Modify a scheduler to drop everyone with a score below SCORE
-func bound(s Sched, score float64) Sched {
-	return filter(s, func(c *Client) bool {
+func bound(score float64) Sched {
+	return filter(func(c *Client) bool {
 		return c.Score >= score
 	})
+}
+
+//
+func skim(count int) Sched {
+	return func(queue []*Client) ([]*Client, bool) {
+		if len(queue) < count {
+			return queue, true
+		}
+
+		sort.Slice(queue, func(i, j int) bool {
+			return queue[i].Score > queue[j].Score
+		})
+
+		n := count
+		for n < len(queue) && queue[count-1].Score == queue[n].Score {
+			n++
+		}
+		return queue[:n], true
+	}
 }
 
 var random Sched = func(queue []*Client) ([]*Client, bool) {
@@ -334,81 +320,64 @@ func schedule(sched Sched) {
 
 // Parse a scheduler specification into a scheduler
 //
-// Each specification is a list of tokens operating on a scheduler
-// stack.
-//
-// The simplest case is a single word denoting a primitive scheduler:
-//
-//    "random"
-//    "round-robin"
-//
-// A more complicated example might combine schedulers.  For example
-//
-//    "round-robin random compose"
-//
-// will add a round-robin scheduler to the stack, followed by a random
-// scheduler, then combine these into a sequential scheduler that
-// first executes the random scheduler, then the random scheduler.
-//
-// Some tokens may use arguments, separated by periods, to modify
-// their behaviour (here using the abbreviated syntax):
-//
-//    "rr !.1000 $ |.1 > >"
-//
-// Would start a random scheduler, eliminate all clients that lost a
-// random match (ie. have a score of 0, as opposed to 1), proceed to
-// reset the score for all remaining clients and finally start a
-// round-robin tournament.
+// The scheduler specification is described in the manual.
 func parseSched(spec string) Sched {
-	var st []Sched
+	var scheds []Sched
 
-	for i, word := range strings.Split(spec, " ") {
+	for _, word := range strings.Split(spec, " ") {
 		parse := strings.Split(word, ".")
 		switch parse[0] {
-		// Scheduler primitives
 		case "fifo":
-			st = append(st, fifo)
-		case "rand", "random", "$":
-			st = append(st, random)
-		case "noop":
-			st = append(st, noop)
+			scheds = append(scheds, fifo)
+		case "rand", "random":
+			scheds = append(scheds, random)
 		case "rr", "round-robin":
-			st = append(st, makeTournament(&roundRobin{}))
-
-			// Scheduler combinators
-		case "seq", "compose", "+", ">":
-			if len(st) < 2 {
-				log.Fatal("Stack underflow at", i)
+			if len(parse) != 2 {
+				log.Fatal("Round robin requires a board sizes")
 			}
-			a := st[len(st)-1]
-			b := st[len(st)-2]
-			st = append(st[:len(st)-2], compose(a, b))
-		case "limit", "=":
 			n, err := strconv.Atoi(parse[1])
 			if err != nil || n < 0 {
-				log.Fatal("Invalid limit", parse[1])
+				log.Fatal("Invalid size", parse[1])
 			}
-			s := limit(st[len(st)-1], uint(n))
-			st = append(st[:len(st)-1], s)
-		case "bound", "filter", "|":
+			rr := makeTournament(&roundRobin{})
+			scheds = append(scheds, rr)
+		case "bound":
 			score, err := strconv.ParseFloat(parse[1], 64)
 			if err != nil || score < 0 {
 				log.Fatal("Invalid score", parse[1])
 			}
-			st = append(st[:len(st)-1], bound(st[len(st)-1], score))
+			scheds = append(scheds, bound(score))
+		case "skim":
+			count, err := strconv.Atoi(parse[1])
+			if err != nil || count <= 0 {
+				log.Fatal("Invalid count", parse[1])
+			}
+			scheds = append(scheds, skim(count))
 		case "reset", "!":
+			if len(parse) != 2 {
+				log.Fatal("reset requires an argument")
+			}
 			score, err := strconv.ParseFloat(parse[1], 64)
 			if err != nil || score < 0 {
 				log.Fatal("Invalid score", parse[1])
 			}
-			st = append(st[:len(st)-1], reset(score))
+			scheds = append(scheds, reset(score))
 		default:
 			log.Fatal("Unknown word", word)
 		}
 	}
 
-	if len(st) == 0 {
+	switch len(scheds) {
+	case 0:
 		log.Fatal("Failed to parse scheduler spec")
+		return nil
+	case 1:
+		return scheds[0]
+	default:
+		for i := 0; i < len(scheds)/2; i++ {
+			j := len(scheds) - i - 1
+			scheds[i], scheds[j] = scheds[j], scheds[i]
+		}
+		return compose(scheds)
 	}
-	return st[len(st)-1] // Pop top-of-stack
 }
