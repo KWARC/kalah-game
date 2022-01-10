fa9a2b10a02370a84b8217d9d0926ec5a0ca354a
Philip Kaludercic <philip.kaludercic@fau.de>
2022-01-10 22:48:43 +0100

    go-kgp: Remove the scheduler remove function
    With the simplified assumption that the queue contains no duplicates,
    there is no need for the more complex remove function.

--- server/go-kgp/sched.go
+++ server/go-kgp/sched.go
@@ -21,7 +21,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	"math/rand"
 	"sort"
@@ -226,66 +225,6 @@ var fifo Sched = func(queue []*Client) ([]*Client, bool) {
 	return queue, false
 }
 
-func remove(queue []*Client, cli *Client) []*Client {
-	// Traverse the queue and replace any reference to CLI with a nil
-	// pointer.
-	found := false
-	for i := range queue {
-		if queue[i] == nil {
-			panic(fmt.Sprintf("Nil reference found in queue (%d): %v", i, queue))
-		}
-		if queue[i] == cli {
-			queue[i] = nil
-			found = true
-		}
-	}
-
-	// In case CLI was not in the queue, we return it unmodified
-	if !found {
-		return queue
-	}
-
-	// To avoid copying the contents of the entire queue, we will just
-	// pull clients from the back of the queue to the beginning.  The
-	// queue therefore is does not preserve a real "real" FIFO
-	// ordering, but this is not necessary and might be argued to
-	// introduce some dynamism into the system.
-
-	// We traverse the queue from the beginning to the end, pulling
-	// clients from the end of the queue to current position (i) to
-	// fill empty spots.
-	for i := 0; i < len(queue); i++ {
-
-		// Before checking if the entry at the current position is
-		// nil, we want to ensure that the last element in the queue
-		// is not nil.
-		for len(queue) > i && queue[len(queue)-1] == nil {
-			queue = queue[:len(queue)-1]
-		}
-
-		// In case we have removed all elements, we can stop immediately
-		if len(queue) == 0 {
-			break
-		}
-
-		// If the current element is empty, we have either reached the
-		// end of the queue when LEN == I, or there is an element at
-		// the end of the queue (QUEUE[LEN-1]) that can be pulled
-		// ahead.
-		if queue[i] == nil {
-			if len(queue) == i {
-				queue = queue[:len(queue)-1]
-				break
-			} else {
-				queue[i] = queue[len(queue)-1]
-				queue = queue[:len(queue)-1]
-			}
-		}
-	}
-
-	return queue
-}
-
 // Using a scheduler, handle incoming events (requests to add and
 // remove clients from the queue), to start games.
 func schedule(sched Sched) {
@@ -306,7 +245,12 @@ func schedule(sched Sched) {
 				queue = append(queue, cli)
 			}
 		case cli := <-forget:
-			queue = remove(queue, cli)
+			for i, c := range queue {
+				if cli == c {
+					queue = append(queue[:i], queue[i+1:]...)
+					break
+				}
+			}
 		}
 
 		// Attempt to organise a match
