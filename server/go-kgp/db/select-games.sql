47f9925828cd4be6975271b6702d0c8cb26b9b5b
Philip Kaludercic <philip.kaludercic@fau.de>
2022-12-11 13:40:21 +0100

    [go-kgp] Order by game id instead of last move when selecting games

--- server/go-kgp/db/select-games.sql
+++ server/go-kgp/db/select-games.sql
@@ -4,6 +4,6 @@ SELECT game.id, game.size, game.init, game.north, game.south, game.outcome,
        COUNT(move.game)
 FROM game INNER JOIN move ON game.id = move.game
 GROUP BY game.id
-ORDER BY MAX(move.played) DESC
+ORDER BY game.id DESC
 LIMIT 25
 OFFSET ?1 * 100;
