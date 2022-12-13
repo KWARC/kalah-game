fc5ed19f6a211d8ce2a6f7f3cbe6b7e4b9ae5e02
Philip Kaludercic <philip.kaludercic@fau.de>
2022-12-13 18:18:40 +0100

    [go-kgp] Add SQLite checks on game.{size,init} to ensure positivity

--- server/go-kgp/db/create-game.sql
+++ server/go-kgp/db/create-game.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE IF NOT EXISTS game (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	size INTEGER,
-	init INTEGER,
+	size INTEGER CHECK(size > 0) NOT NULL,
+	init INTEGER CHECK(init > 0) NOT NULL,
 	north REFERENCES agent(id),
 	south REFERENCES agent(id),
 	state TEXT CHECK(state IN ("o", "nw", "sw", "u", "nr", "sr", "a"))
