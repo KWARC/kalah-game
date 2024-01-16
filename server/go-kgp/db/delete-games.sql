2bfc76a599b40ead739025b675393f3f933cb123
Philip Kaludercic <philip.kaludercic@fau.de>
2024-01-16 08:12:28 +0100

    [go-kgp] Use time modifier "day" instead of "week"
    See https://www.sqlite.org/lang_datefunc.html#modifiers, and notice
    that there is no modified "week".  The consequence is that 'strftime'
    returns an empty string, the comparison with MAX(move.played) is never
    satisfied and hence no old games are deleted.

--- server/go-kgp/db/delete-games.sql
+++ server/go-kgp/db/delete-games.sql
@@ -3,5 +3,5 @@
 WITH forget AS (SELECT move.game FROM move GROUP BY move.game
      	        HAVING MAX(move.played)
 		     < strftime("%Y-%m-%d %H:%M:%S.000000000+00:00",
-		                "now", "-1 week"))
+		                "now", "-7 day"))
 DELETE FROM game WHERE game.id IN forget;
